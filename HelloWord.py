a58a58cd4a2479ef96cd10a245a206e665f6a125
lfmos <lfmedeiros.rj@gmail.com>
2025-08-13 11:31:38 -0300

    Add files via upload

---
+++ HelloWord.py
@@ -0,0 +1,5 @@
+# Este será o meu primeiro programa de computador!
+print("Olá mundo!")
+print('I\'m not the person that you think that i am!')
+print("But, I'm could e the person that you want.") 
+print('If you think that I\'m really "special"!')
